461592ef6fec34ff84e54ed2217e4b5a278b764d
ms2sato <ms2.sato@gmail.com>
2014-12-29 21:57:11 +0900

    トップなど調整

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   has_many :comments, foreign_key: 'parent_id'
   belongs_to :parent, class_name: 'Comment', foreign_key: 'parent_id', touch: true
+
+  scope :all_parents, ->{
+    where(parent_id:nil)
+  }
+
 end
